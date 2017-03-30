a48eb4b93c85b252728283bc01a88fd36aa09d29
Sv-Shok <svshok1@gmail.com>
2017-03-30 18:54:57 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
         myHeader.hide(2000);
-        myHeader.slideDown(3000);      
+          myHeader.show(3000);    
 
         });
+  //myHeader.slideDown(3000); 
